a63eb300160b8e1867ca60d0e6346a6fb4a43861
Md.siamul islam shayon <69626845+shayon10@users.noreply.github.com>
2024-03-12 02:13:21 +0600

    Update text_converter_app.py

--- text_converter_app.py
+++ text_converter_app.py
@@ -24,7 +24,7 @@ class TextConverterApp:
 
         # Tokenize and generate text with lower temperature
         input_ids = tokenizer.encode(ai_text, return_tensors="pt")
-        output = model.generate(input_ids, max_length=150, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
+        output = model.generate(input_ids, max_length=150, num_beams=5, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7, max_new_tokens=500)
 
         # Decode and display the converted text
         human_text = tokenizer.decode(output[0], skip_special_tokens=True)
